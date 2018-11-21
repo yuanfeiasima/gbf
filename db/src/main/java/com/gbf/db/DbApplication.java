6d42e8ead4dc881edd0eeca45703c2b59c64f6e6
yuanfeiasima <382810567@qq.com>
2018-11-21 18:39:35 +0800

    first commit

---
+++ db/src/main/java/com/gbf/db/DbApplication.java
@@ -0,0 +1,12 @@
+package com.gbf.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbApplication.class, args);
+    }
+}
